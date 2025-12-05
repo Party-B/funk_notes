0fa217ce88a40e6f1ddefb13700412ccbe75b179
lenovo <ghostbones@lenovo.localdomain>
2025-12-05 16:13:30 +1100

    Starting to implement a txt file storage

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,29 @@ use crate::timestamp::now_timestamp;
 use std::io;
 
 // Constants
-const EXIT_COMMANDS: &[&str] = &["exit", "quit", "q", "drop"];
+const EXIT_CMDS: &[&str] = &["exit", "quit", "q", "drop"];
 
 fn main() {
-    let mut input = String::new();
-    println!("Enter command (or 'shell' for shell mode):");
-    std::io::stdin().read_line(&mut input).unwrap();
-    let input = input.trim();
 
-    if input == "shell" {
-        // Shell mode loop
+    // Collect cli args
+    let args = std::env::args()
+        .skip(1)
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    // Assess if the first word is the shell command
+    if args.split_whitespace().next() == Some("shell") {
+        
+        // Enter the shell loop
         loop {
+
             let mut shell_input = String::new();
             print!("funk_notes> ");
             //std::io::stdout().flush().unwrap();
             std::io::stdin().read_line(&mut shell_input).unwrap();
             let shell_input = shell_input.trim();
 
-            if EXIT_COMMANDS.contains(&shell_input) {
+            if EXIT_CMDS.contains(&shell_input) {
                 println!("Exiting shell mode.");
                 break;
             }
@@ -34,7 +39,7 @@ fn main() {
         }
     } else {
         // Single command mode
-        match parse_command(input) {
+        match parse_command(&args) {
             Some(cmd) => execute_command(cmd),
             None => println!("No valid command."),
         }
@@ -66,6 +71,7 @@ fn execute_command(cmd: Command) {
         _ => println!("No valid command."),
     }
 }
+
 
 
 
--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,15 @@
 
 use crate::timestamp::now_timestamp;
-use std::fs;
 use std::path::Path;
+use std::fs::{self, OpenOptions};
+use std::io::{self, Write};
+use std::env;
 
-const COUNTER_FILE: &str = "data/funk_metadata.txt";
+// Constants
+const METADATA_FILE: &str = "data/funk_metadata.txt";
+const SPLIT_CODE: &str = "(note.id";
+const FILE_PATH: &str = "all_notes.txt";
+
 
 pub enum Command {
     New { title: String, description: String },
@@ -53,11 +59,11 @@ impl Funknote {
 /// Read next ID from counter file, increment file, return the ID.
 pub fn get_next_id() -> Result<usize, std::io::Error> {
     // Make sure file exists. If not, create it with "1".
-    if !Path::new(COUNTER_FILE).exists() {
-        fs::write(COUNTER_FILE, "1").expect("Failed to create counter file");
+    if !Path::new(METADATA_FILE).exists() {
+        fs::write(METADATA_FILE, "1").expect("Failed to create counter file");
     }
 
-    let contents = fs::read_to_string(COUNTER_FILE)
+    let contents = fs::read_to_string(METADATA_FILE)
         .expect("Failed to read counter file");
 
     let current: usize = contents.trim().parse()
@@ -65,9 +71,50 @@ pub fn get_next_id() -> Result<usize, std::io::Error> {
 
     // Increment and save
     let next = current + 1;
-    fs::write(COUNTER_FILE, next.to_string())
+    fs::write(METADATA_FILE, next.to_string())
         .expect("Failed to write counter file");
 
     Ok(current)
 }
+
+fn formatted_string(id: i64, title: &str) -> String {
+    // Using a raw string literal - no escaping needed
+   format!( r#"
+(note.id{id}.start)
+
+{id}.title: {title}
+
+{id}.description:
+
+{id}.date:
+
+(note.id{id}.end)
+"#)
+}
+
+fn append_to_file(file_path: &str, content: &str) -> io::Result<()> {
+    let mut file = OpenOptions::new()
+        .create(true)   // Creates if doesn't exist
+        .append(true)   // Appends if does exist
+        .open(file_path)?;
+    
+    file.write_all(content.as_bytes())?;
+    Ok(())
+}
+fn read_from_file(file_path: &str) -> io::Result<String> {
+    fs::read_to_string(file_path)
+}
+fn chunked(text: &str, chunkno: i64) -> Option<String> {
+    if chunkno < 1 { return None }
+    
+    text.split(SPLIT_CODE)
+        .nth(chunkno as usize)
+        .map(|s| s.to_string())
+}
+
+fn next_chunk(file: &str) -> io::Result<i64> {
+    let content = read_from_file(file).unwrap_or_default();  // Empty string if error
+    Ok(content.split(SPLIT_CODE).count() as i64)
+}
+
 
